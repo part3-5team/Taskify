610db21b8b62e3585d971768455b3a46de038103
isuzzi <isuzzi11@gmail.com>
2026-04-22 14:47:35 +0900

    feat: dashboard header css 수정

--- components/dashboard/Header.tsx
+++ components/dashboard/Header.tsx
@@ -3,7 +3,7 @@ import UserPlus from '@/assets/icons/ic_user-plus.svg'
 import Link from 'next/link'
 export default function Header() {
   return (
-    <div className="bg-black-400 flex h-18 w-full items-center justify-end p-7.5">
+    <div className="bg-black-400 border-black-300 flex h-18 w-full items-center justify-end border-b-2 p-7.5">
       <div className="text-lg-16-medium flex gap-4 text-gray-400">
         {/* TODO : Link href 수정 */}
         <Link
