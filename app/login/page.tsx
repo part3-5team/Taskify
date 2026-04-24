313b38dd3c4aabef36be1191e0474421f940ac0a
Hanpla <hanpla25@gmail.com>
2026-04-24 15:12:41 +0900

    chore: 임시 로고 정식 로고로 변경

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import LogoImg from '@/assets/imgs/img_Logo.svg'
 import AuthLayout from '@/components/auth/AuthLayout'
 import LoginForm from '@/components/auth/LoginForm'
 import LoginLink from '@/components/auth/LoginForm/LoginLink'
@@ -8,7 +9,9 @@ export default function LoginPage() {
     <AuthLayout>
       <div className="mx-auto max-w-130 pt-50">
         <div className="mb-7.5 text-center text-white">
-          <Link href="/">로고</Link>
+          <Link href="/" className="inline-block w-fit">
+            <LogoImg className="mx-auto h-19.25 w-75 md:h-21.75 md:w-85" />
+          </Link>
         </div>
         <LoginForm />
         <LoginLink />
