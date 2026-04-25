d7481f82df30f30f0055d8379ce019be3f624038
Hanpla <hanpla25@gmail.com>
2026-04-25 13:10:00 +0900

    feat: 로그인 시 토큰 저장 추가

--- libs/api/auth.ts
+++ libs/api/auth.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import { redirect } from 'next/navigation'
+import { cookies } from 'next/headers'
 import { LoginState } from '../types/Auth'
 
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL
@@ -18,12 +19,23 @@ export const login = async (
     headers: { 'Content-Type': 'application/json' },
   })
 
+  const data = await response.json()
+
   if (!response.ok) {
     return {
       success: false,
       message: '이메일 또는 비밀번호가 일치하지 않습니다.',
     }
   }
+
+  const cookieStore = await cookies()
+  cookieStore.set('accessToken', data.accessToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'lax',
+    path: '/',
+    maxAge: 60 * 60 * 24 * 7,
+  })
 
   redirect('/mydashboard')
 }
