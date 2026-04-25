a8195214695b9ca053c2993154bb1b86c811068c
Hanpla <hanpla25@gmail.com>
2026-04-25 13:57:24 +0900

    chore: auth 로고 컴포넌트 분리

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,15 +1,13 @@
-import Link from 'next/link'
 import AuthLayout from '@/components/auth/AuthLayout'
 import SignupForm from '@/components/auth/SignupForm'
 import SignupLink from '@/components/auth/SignupForm/SignupLink'
+import AuthLogo from '@/components/auth/AuthLayout/AuthLogo'
 
 export default function SignupPage() {
   return (
     <AuthLayout>
       <div className="mx-auto max-w-130 pt-50">
-        <div className="mb-7.5 text-center text-white">
-          <Link href="/">로고</Link>
-        </div>
+        <AuthLogo />
         <SignupForm />
         <SignupLink />
       </div>
