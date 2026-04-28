9c4a718147bbb085433c48343753bc533b085927
Haning <100738360+Choiyuhyeon@users.noreply.github.com>
2026-04-28 16:20:55 +0900

    feat: axios 설치 및 대시보드 목록조회 api 추가

--- libs/api/dashboard/getDashboard.ts
+++ libs/api/dashboard/getDashboard.ts
@@ -1,10 +1,12 @@
 'use server'
 
-import { ApiResult } from '@/libs/types/Api'
-import { GetDashboardsResponse } from '@/libs/types/Dashboard'
 import { cookies } from 'next/headers'
+import axios from 'axios'
+import { ApiResult } from '@/libs/types/Api'
+import { Dashboard, GetDashboardsResponse } from '@/libs/types/Dashboard'
 
 interface GetDashboardsParams {
+  navigationMethod?: 'pagination' | 'infiniteScroll'
   page?: number
   size?: number
   cursorId?: number | null
@@ -12,7 +14,9 @@ interface GetDashboardsParams {
 
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL
 
+// 대시보드 목록 조회
 export const getDashboards = async ({
+  navigationMethod = 'pagination',
   page = 1,
   size = 9,
   cursorId,
@@ -21,7 +25,7 @@ export const getDashboards = async ({
     const cookieStore = await cookies()
     const token = cookieStore.get('accessToken')?.value
     const params = new URLSearchParams({
-      navigationMethod: 'infiniteScroll',
+      navigationMethod,
       page: page.toString(),
       size: size.toString(),
     })
@@ -56,6 +60,46 @@ export const getDashboards = async ({
     }
   } catch (error) {
     console.log(error)
+
+    return {
+      success: false,
+      data: null,
+      error: '네트워크 연결 상태를 확인해주세요.',
+    }
+  }
+}
+
+// 대시보드 단일 조회
+export const getDashboardById = async (
+  dashboardId: number,
+): Promise<ApiResult<Dashboard>> => {
+  try {
+    const cookieStore = await cookies()
+    const token = cookieStore.get('accessToken')?.value
+
+    const { data } = await axios.get<Dashboard>(
+      `${BASE_URL}/dashboards/${dashboardId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    )
+
+    return {
+      success: true,
+      data,
+      error: null,
+    }
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return {
+        success: false,
+        data: null,
+        error:
+          error.response?.data?.message || '대시보드를 불러오지 못했습니다.',
+      }
+    }
 
     return {
       success: false,
