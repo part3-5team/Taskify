e556d7280adbb99cabf60a268a788c56eae569d7
isuzzi <isuzzi11@gmail.com>
2026-04-30 12:51:57 +0900

    feat: 초대받은 대시보드 반응형 수정

--- components/mydashboard/InvitationList.tsx
+++ components/mydashboard/InvitationList.tsx
@@ -12,10 +12,11 @@ export default function InvitationList({
   console.log(invitations)
   return (
     <div>
-      <div className="border-black-200 text-2lg-18-bold hidden items-center justify-center gap-4 border-b px-7.5 py-3.5 md:flex md:gap-2.5">
-        <p className="flex-6 justify-center text-gray-400">이름</p>
-        <p className="flex-2 justify-center text-gray-400">초대자</p>
-        <p className="flex-2 justify-center text-gray-400">수락 여부</p>
+      {/* 반응형에서 보이지 않게 처리 */}
+      <div className="border-black-200 text-2lg-18-bold hidden border-b px-7.5 py-3.5 text-gray-400 md:grid md:grid-cols-[minmax(0,3fr)_minmax(0,2fr)_minmax(112px,1fr)] md:items-center md:gap-4">
+        <p className="text-center">이름</p>
+        <p className="text-center">초대자</p>
+        <p className="text-center">수락 여부</p>
       </div>
       {invitations.map((invitation) => {
         const inviter = invitation.inviter
@@ -23,14 +24,16 @@ export default function InvitationList({
         return (
           <div
             key={invitation.id}
-            className="border-black-200 items-center border-b px-7.5 py-3.5 md:flex"
+            className="border-black-200 border-b px-7.5 py-3.5 md:grid md:grid-cols-[minmax(0,3fr)_minmax(0,2fr)_minmax(112px,1fr)] md:items-center md:gap-4"
           >
-            <p className="text-2lg-18-bold min-w-0 truncate md:flex-[3]">
+            <p className="text-2lg-18-bold min-w-0 truncate">
               {invitation.dashboard.title}
             </p>
-            <div className="flex w-full items-center justify-between gap-4 md:flex-[2]">
-              <p className="min-w-0 truncate md:flex-1">{inviter.nickname}</p>
-              <div className="text-lg-16-semibold flex shrink-0 gap-2 text-white">
+            <div className="flex items-center justify-between gap-4 md:contents">
+              <p className="mt-1 min-w-0 truncate text-gray-300 md:mt-0 md:text-center">
+                {inviter.nickname}
+              </p>
+              <div className="text-lg-16-semibold flex shrink-0 justify-end gap-2 text-white md:justify-center">
                 {/* 받은 초대 거절/수락 */}
                 <button
                   type="button"
